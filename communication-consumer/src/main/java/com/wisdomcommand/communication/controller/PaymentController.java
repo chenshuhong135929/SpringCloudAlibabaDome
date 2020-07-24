b6c42e682d63cc7b722c1e27d601fdfa168326f3
chenshuhong135929 <1048369112@qq.com>
2020-07-24 09:47:24 +0800

    添加事件回掉示例

--- communication-consumer/src/main/java/com/wisdomcommand/communication/controller/PaymentController.java
+++ communication-consumer/src/main/java/com/wisdomcommand/communication/controller/PaymentController.java
@@ -8,6 +8,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +25,23 @@ public class PaymentController {
   @Resource
   private PaymentService paymentService;
 
+  @Autowired
+  ApplicationEventPublisher applicationEventPublisher;
+
   @ApiOperation(value = "调试")
   @GetMapping(value = "/paymentSQL/{id}")
   @ApiImplicitParam(name = "id",value = "主键ID", required = true,paramType = "path")
   public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id)
   {
-    return paymentService.paymentSQL(id);
+    CommonResult<Payment> paymentCommonResult = paymentService.paymentSQL(id);
+    applicationEventPublisher.publishEvent(paymentCommonResult.getData());
+    return paymentCommonResult;
   }
+//事件回掉示例
+  @EventListener
+  @Async
+  public void  evetPayment(Payment e){
+   System.out.println("事件回掉  "+e.getSerial());
+  }
+
 }
